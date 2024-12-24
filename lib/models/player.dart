5c47752d49b3d9c3ae66b9dd13f25e0fcdab54ff
Waleed Ahmed <waleedahmedja@gmail.com>
2024-12-24 19:30:01 +0500

    Update player.dart

---
+++ lib/models/player.dart
@@ -0,0 +1,18 @@
+class Player {
+  final String name;
+  int runs = 0;
+  int ballsFaced = 0;
+  int fours = 0;
+  int sixes = 0;
+  int wickets = 0;
+  int ballsBowled = 0;
+  int runsConceded = 0;
+
+  Player({required this.name});
+
+  // Calculate strike rate
+  double get strikeRate => ballsFaced == 0 ? 0 : (runs / ballsFaced) * 100;
+
+  // Calculate bowling economy
+  double get economyRate => ballsBowled == 0 ? 0 : (runsConceded / ballsBowled) * 6;
+}
