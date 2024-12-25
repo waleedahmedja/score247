4f6c40ebfa0969445f8ff9dd417c91431b403a5f
Waleed Ahmed <waleedahmedja@gmail.com>
2024-12-25 13:41:52 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -20,3 +20,29 @@ class Score247App extends StatelessWidget {
     );
   }
 }
+import 'package:flutter/material.dart';
+import 'screens/setup_screen.dart';
+import 'screens/toss_screen.dart';
+import 'screens/match_screen.dart';
+
+void main() {
+  runApp(const Score247App());
+}
+
+class Score247App extends StatelessWidget {
+  const Score247App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Score247',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const SetupScreen(),
+        '/toss': (context) => const TossScreen(),
+        '/match': (context) => const MatchScreen(),
+      },
+    );
+  }
+}
