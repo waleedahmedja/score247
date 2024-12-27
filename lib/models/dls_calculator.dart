3389dfcfe869503a916f3708d1e97111a2aa1fcb
Waleed Ahmed <waleedahmedja@gmail.com>
2024-12-27 16:35:50 +0500

    Update dls_calculator.dart

--- lib/models/dls_calculator.dart
+++ lib/models/dls_calculator.dart
@@ -1,4 +1,3 @@
-// dls_calculator.dart
 class DLSCalculator {
   // Basic resource percentage table
   static const Map<int, double> resourcesTable = {
@@ -15,17 +14,42 @@ class DLSCalculator {
     0: 0.0,
   };
 
+  // Linear interpolation for resource percentage
+  static double getResourcePercentage(int oversRemaining) {
+    if (resourcesTable.containsKey(oversRemaining)) {
+      return resourcesTable[oversRemaining]!;
+    }
+
+    // Find closest lower and upper bounds for oversRemaining
+    final lowerKey = resourcesTable.keys.where((k) => k < oversRemaining).reduce((a, b) => a > b ? a : b);
+    final upperKey = resourcesTable.keys.where((k) => k > oversRemaining).reduce((a, b) => a < b ? a : b);
+
+    // Interpolate between lower and upper bounds
+    final lowerValue = resourcesTable[lowerKey]!;
+    final upperValue = resourcesTable[upperKey]!;
+    final fraction = (oversRemaining - lowerKey) / (upperKey - lowerKey);
+
+    return lowerValue + (upperValue - lowerValue) * fraction;
+  }
+
   // Calculate target score
   static int calculateTarget(
     int team1Score,
     int oversRemaining,
-    double resourceRemaining,
   ) {
-    final double team1Resources = resourcesTable[50]!; // Assume Team 1 batted full 50 overs
-    final double team2Resources = resourceRemaining;
+    // Validate inputs
+    if (oversRemaining < 0 || oversRemaining > 50) {
+      throw ArgumentError("Overs remaining must be between 0 and 50");
+    }
+
+    // Team 1 resources are always based on 50 overs (max resources)
+    final double team1Resources = resourcesTable[50]!;
+
+    // Get Team 2's remaining resources
+    final double team2Resources = getResourcePercentage(oversRemaining);
 
     // Calculate resource percentage
-    double resourcePercentage = team2Resources / team1Resources;
+    final double resourcePercentage = team2Resources / team1Resources;
 
     // Adjusted target score
     return (team1Score * resourcePercentage).ceil();
